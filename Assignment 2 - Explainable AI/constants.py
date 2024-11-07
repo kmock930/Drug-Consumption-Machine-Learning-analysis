9234eb2b78e7ad6a2e685a20d6bfd2638df856c1
Kelvin Mock <kelvin.mock@gmail.com>
2024-11-06 23:46:39 -0500

    Created Project Structure for Assignment 2

---
+++ Assignment 2 - Explainable AI/constants.py
@@ -0,0 +1,20 @@
+ASM1_DIR = "../Assignment 1 - Model Comparison/";
+filepaths = {
+    # posttrainned models from assignment 1
+    "choc_posttrained_decisionTree": "choc\posttrained\choc_model_decisionTree_posttrained.pkl",
+    "choc_posttrained_SVC": ".\choc\posttrained\choc_model_SVC_RBF_posttrained.pkl",
+    "mushrooms_posttrained_MLP": ".\mushrooms\posttrained\mushrooms_model_MLP_posttrained.pkl",
+    "mushrooms_posttrained_SVC": ".\mushrooms\posttrained\mushrooms_model_SVC_RBF_posttrained.pkl",
+    #choc test set
+    "choc_test-set_labels": ".\choc\Test Set\choc_test-set_labels.pkl",
+    "choc_test-set_samples": ".\choc\Test Set\choc_test-set_samples.pkl",
+    #mushrooms test set
+    "mushrooms_test-set_labels": ".\mushrooms\Test Set\mushrooms_test-set_labels.pkl",
+    "mushrooms_test-set_samples": ".\mushrooms\Test Set\mushrooms_test-set_samples.pkl",
+    # choc training set
+    "choc_train-set_labels": ".\choc\Training Set\choc_train-set_labels.pkl",
+    'choc_train-set_samples': ".\choc\Training Set\choc_train-set_samples.pkl",
+    # mushrooms training set
+    "mushrooms_train-set_labels": ".\mushrooms\Training Set\mushrooms_train-set_labels.pkl",
+    'mushrooms_train-set_samples': ".\mushrooms\Training Set\mushrooms_train-set_samples.pkl",
+};
---
+++ Assignment 2 - Explainable AI/fileOrganizer.py
@@ -0,0 +1,78 @@
+'''
+This is a script that executes to 
+move specific .pkl model files into proper directories.
+'''
+import os;
+import shutil;
+import constants;
+
+import os;
+import joblib;
+
+def unpack(filepath):
+    with open(filepath, 'rb') as file:
+        return joblib.load(file);
+
+def organize():
+    # Define the root directory where the model files are located
+    root_dir = './';
+
+    # Dataset name extracted from file names (in this case "choc")
+    datasets = [constants.choco_dataset, constants.mushrooms_dataset];
+
+    for dataset_name in datasets:
+        # Create dataset directory
+        dataset_dir = os.path.join(root_dir, dataset_name);
+        if not os.path.exists(dataset_dir):
+            os.makedirs(dataset_dir);
+
+        # Create subdirectories for trained and pre-trained models
+        trained_dir = os.path.join(dataset_dir, 'posttrained');
+        pretrained_dir = os.path.join(dataset_dir, 'pretrained');
+        train_set_dir = os.path.join(dataset_dir, 'Training Set');
+        test_set_dir = os.path.join(dataset_dir, 'Test Set');
+
+        if not os.path.exists(trained_dir):
+            os.makedirs(trained_dir);
+
+        if not os.path.exists(pretrained_dir):
+            os.makedirs(pretrained_dir);
+        
+        if not os.path.exists(train_set_dir):
+            os.makedirs(train_set_dir);
+        
+        if not os.path.exists(test_set_dir):
+            os.makedirs(test_set_dir);
+
+        # List of all files in the root directory
+        files = os.listdir(root_dir);
+
+        # Loop through the files and move them to the appropriate folder
+        for file in files:
+            if (file.endswith('.pkl') and dataset_name in file): # organize all .pkl files
+                if (file.startswith(f"{dataset_name}_model")):
+                    if 'pretrained' in file:
+                        # Move to pretrained folder
+                        src_path = os.path.join(root_dir, file);
+                        dest_path = os.path.join(pretrained_dir, file);
+                        shutil.move(src_path, dest_path);
+                        print(f"Moved {file} to {os.path.join(pretrained_dir)}");
+                    elif 'posttrained' in file:
+                        # Move to trained folder
+                        src_path = os.path.join(root_dir, file);
+                        dest_path = os.path.join(trained_dir, file);
+                        shutil.move(src_path, dest_path);
+                        print(f"Moved {file} to {os.path.join(trained_dir)}");
+                elif ('train-set' in file): # training set data
+                    src_path = os.path.join(root_dir, file);
+                    dest_path = os.path.join(train_set_dir, file);
+                    shutil.move(src_path, dest_path);
+                    print(f"Moved {file} to {os.path.join(train_set_dir)}");
+                elif ('test-set' in file): # test set data
+                    src_path = os.path.join(root_dir, file);
+                    dest_path = os.path.join(test_set_dir, file);
+                    shutil.move(src_path, dest_path);
+                    print(f"Moved {file} to {os.path.join(test_set_dir)}");
+
+
+    print("Organization complete!");

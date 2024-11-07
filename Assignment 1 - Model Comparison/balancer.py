f0fed7f131ebf5009e54206e26f99938b3eabb87
Kelvin Mock <kelvin.mock@gmail.com>
2024-11-06 19:40:46 -0500

    Updated Combined Sampling approach

--- Assignment 1 - Model Comparison/balancer.py
+++ Assignment 1 - Model Comparison/balancer.py
@@ -2,6 +2,7 @@ import constants;
 import numpy as np;
 from imblearn.under_sampling import RandomUnderSampler;
 from imblearn.over_sampling import SMOTE;
+from imblearn.pipeline import Pipeline;
 
 rus = RandomUnderSampler(
     sampling_strategy=constants.underSamplePercent,
@@ -17,3 +18,7 @@ def underSample(X_train: np.ndarray, y_train: np.ndarray):
 
 def overSample(X_train: np.ndarray, y_train: np.ndarray):
     return smote.fit_resample(X_train, y_train);
+
+def combineSample(X_train: np.ndarray, y_train: np.ndarray):
+    pipeline = Pipeline(steps=[('undersampling', rus), ('oversampling', smote)]);
+    return pipeline.fit_resample(X_train, y_train);

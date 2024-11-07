e7954d6294b9d2e11859a0c4dcf230cb4ba92eed
Kelvin Mock <kelvin.mock@gmail.com>
2024-11-07 15:33:12 -0500

    Calculated SHAP values for 4 selected classifiers on both datasets
    by providing dedicated methods for different types of classifiers, loading the calculation results into .npy files for easier debugging and further work, as well as creating a new notebook for ongoing progress.

--- Assignment 2 - Explainable AI/explainer.py
+++ Assignment 2 - Explainable AI/explainer.py
@@ -1,4 +1,5 @@
 import shap;
+import shap.maskers
 from sklearn.tree import DecisionTreeClassifier;
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier;
 from sklearn.svm import SVC;
@@ -6,14 +7,16 @@ from sklearn.neural_network import MLPClassifier;
 from sklearn.neighbors import KNeighborsClassifier;
 from sklearn.model_selection import RandomizedSearchCV;
 import numpy as np;
+import constants;
 import traceback;
 
 class explainer:
     treeExplainer: shap.TreeExplainer = None; # for decision tree or random forest
-    kernelExplainer: shap.KernelExplainer = None; # for SVM and MLP
+    linearExplainer: shap.LinearExplainer = None; # for SVM
+    kernelExplainer: shap.SamplingExplainer = None; # for MLP
     model: DecisionTreeClassifier | RandomForestClassifier | GradientBoostingClassifier | SVC | MLPClassifier | KNeighborsClassifier | RandomizedSearchCV;
 
-    def __init__(self, model: DecisionTreeClassifier | RandomForestClassifier | GradientBoostingClassifier | SVC | MLPClassifier | KNeighborsClassifier | RandomizedSearchCV, X_train: np.ndarray, modelType: str = ""):
+    def __init__(self, model: DecisionTreeClassifier | RandomForestClassifier | GradientBoostingClassifier | SVC | MLPClassifier | KNeighborsClassifier | RandomizedSearchCV, data: np.ndarray, modelType: str = ""):
         print("Start instantiating an explainer.");
         self.model = model;
         try:
@@ -21,15 +24,22 @@ class explainer:
                 case "tree":
                     self.treeExplainer = shap.TreeExplainer(
                         model=self.model,
-                        data=X_train
+                        data=data
                     );
                     print("A tree explainer is instantiated successfully.");
-                case "svm" | "mlp":
-                    self.kernelExplainer = shap.KernelExplainer(
-                        model=self.model.predict,
-                        data=X_train
+                case "svm":
+                    self.linearExplainer = shap.LinearExplainer(
+                        model=self.model,
+                        data=data,
+                        masker=shap.maskers.Partition(data)
                     );
-                    print("A kernel explainer is instantiated successfully.");
+                    print("A Linear explainer is instantiated successfully.");
+                case "mlp":
+                    self.kernelExplainer = shap.SamplingExplainer(
+                        model=self.model.predict,
+                        data=data
+                    );
+                    print("A Sampling Kernel explainer is instantiated successfully.");
                 case "":
                     raise ValueError("Missing or Invalid Type of explainer! Please instantiate again.");
         except:
@@ -39,6 +49,9 @@ class explainer:
         if (self.treeExplainer != None):
             print("A tree explainer is found.");
             return self.treeExplainer.shap_values(X_test);
+        elif (self.linearExplainer != None):
+            print("A Linear explainer is found");
+            return self.linearExplainer.shap_values(X_test);
         elif (self.kernelExplainer != None):
             print("A kernel explainer is found");
             return self.kernelExplainer.shap_values(X_test);

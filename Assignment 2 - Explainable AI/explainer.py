49311c7d3f938ef0be6e7f148c80bd3f155c1400
Kelvin Mock <kelvin.mock@gmail.com>
2024-11-08 04:58:44 -0500

    doc

--- Assignment 2 - Explainable AI/explainer.py
+++ Assignment 2 - Explainable AI/explainer.py
@@ -11,6 +11,13 @@ import constants;
 import traceback;
 
 class explainer:
+    '''
+    This is a class which encapsulates all kinds of explainers based on SHAP values of a model.
+
+    Author
+    -----------
+    Kelvin Mock
+    '''
     treeExplainer: shap.TreeExplainer = None; # for decision tree or random forest
     linearExplainer: shap.LinearExplainer = None; # for SVM
     permutationExplainer: shap.PermutationExplainer = None; # for MLP
@@ -18,6 +25,15 @@ class explainer:
     model: DecisionTreeClassifier | RandomForestClassifier | GradientBoostingClassifier | SVC | MLPClassifier | KNeighborsClassifier | RandomizedSearchCV;
 
     def __init__(self, model: DecisionTreeClassifier | RandomForestClassifier | GradientBoostingClassifier | SVC | MLPClassifier | KNeighborsClassifier | RandomizedSearchCV, data: np.ndarray, modelType: str = ""):
+        '''
+        This is a constructor class which instantiates a suitable explainer based on the model's type.
+
+        Parameters
+        ---------------
+        model : DecisionTreeClassifier | RandomForestClassifier | GradientBoostingClassifier | SVC | MLPClassifier | KNeighborsClassifier | RandomizedSearchCV - It allows a raw model or a model which is optimized by Randomized Search.
+        data : numpy.ndarray - Data array used to instantiate an explainer (usually the samples of the training set, unless too large).
+        modelType : str - A string representation of a model's type {"tree", "svm", "neural"}
+        '''
         print("Start instantiating an explainer.");
         self.model = model;
         try:
@@ -52,6 +68,17 @@ class explainer:
             traceback.print_exc();
 
     def explain(self, X_test: np.ndarray):
+        '''
+        This is a function which provides explanation based on a SHAP value.
+
+        Parameters
+        ------------
+        X_test : numpy.ndarray - The data used in an explanation (usually the samples of the test set).
+
+        Returns
+        ------------
+        {numpy.ndarray} - Estimated SHAP values
+        '''
         if (self.treeExplainer != None):
             print("A tree explainer is found.");
             return self.treeExplainer.shap_values(X_test);
@@ -66,3 +93,36 @@ class explainer:
             return self.kernelExplainer.shap_values(X_test);
         else:
             raise ValueError("Missing or Invalid Type of explainer! Please instantiate again.");
+
+    def calBaseVal(self, model: MLPClassifier = None, X_train: np.ndarray = np.array([])):
+        '''
+        This is a function which calculates the base value (for plotting purpose).
+
+        Parameters
+        --------------
+        model : MLPClassifier
+        X_train : numpy.ndarray
+
+        Returns
+        --------------
+        float
+        '''
+        if (self.treeExplainer != None):
+            print("A tree explainer is found.");
+            baseVal = self.treeExplainer.expected_value;
+        elif (self.linearExplainer != None):
+            print("A Linear explainer is found");
+            baseVal = self.linearExplainer.expected_value;
+        elif (self.permutationExplainer != None):
+            print("A Permutation Explainer is found");
+            if (len(X_train) == 0 or model == None):
+                raise ValueError("Missing model or training data. Cannot evaluate the base value.");
+            y_pred_train = model.predict(X_train);
+            baseVal = y_pred_train.mean();
+        elif (self.kernelExplainer != None):
+            print("A Sampling Kernel explainer is found");
+            baseVal = self.kernelExplainer.expected_value;
+        else:
+            raise ValueError("Missing or Invalid Type of explainer! Please instantiate again.");
+
+        return np.mean(baseVal);

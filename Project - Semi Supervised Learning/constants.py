8cb2e9622a560000dadb42586eab501527a019f0
Kelvin Mock <kelvin.mock@gmail.com>
2024-11-13 17:14:50 -0500

    Loaded the baseline model from Assignment 1

--- Project - Semi Supervised Learning/constants.py
+++ Project - Semi Supervised Learning/constants.py
@@ -6,6 +6,18 @@ CHOC_DIR = "choc";
 MUSH_DIR = "mushrooms";
 # data / model type
 POSTTRAINED_DIR = "posttrained";
+TRAIN_DIR = "Training Set";
+TEST_DIR = "Test Set";
+COLUMNS_DIR = "Columns";
 
 # model re-used
 MODEL_FILENAME = "mushrooms_model_Gradient_Boosting_posttrained.pkl";
+
+# data re-used
+X_TRAIN_FILENAME = "mushrooms_train-set_samples.pkl";
+Y_TRAIN_FILENAME = "mushrooms_train-set_labels.pkl";
+X_TEST_FILENAME = "mushrooms_test-set_samples.pkl";
+Y_TEST_FILENAME = "mushrooms_test-set_labels.pkl";
+
+# columns file
+COLUMNS_FILENAME = "mushrooms_columns.pkl";

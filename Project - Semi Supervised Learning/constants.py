47805cf2636c781224af928b243cbdb6c9976afc
Kelvin Mock <kelvin.mock@gmail.com>
2024-11-13 15:36:43 -0500

    Initiated Working Directory for Project

---
+++ Project - Semi Supervised Learning/constants.py
@@ -0,0 +1,11 @@
+# directories
+ASM1_DIR = "Assignment 1 - Model Comparison";
+ASM2_DIR = "Assignment 2 - Explainable AI";
+# datasets
+CHOC_DIR = "choc";
+MUSH_DIR = "mushrooms";
+# data / model type
+POSTTRAINED_DIR = "posttrained";
+
+# model re-used
+MODEL_FILENAME = "mushrooms_model_Gradient_Boosting_posttrained.pkl";
